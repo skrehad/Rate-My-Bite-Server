b76e5a1aaf36482e15d9220ae2d3e6378404ff33
Joy43 <ssjoy43@gmail.com>
2025-05-01 19:51:19 +0600

    router data info

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,6 +3,8 @@ import { authRoute } from "../modules/auth/auth.route";
 import { categoryRoute } from "../modules/category/category.route";
 import { postRoute } from "../modules/post/post.route";
 import { adminRoute } from "../modules/admin/admin.route";
+import { ratingRoutes } from "../modules/Rating/rating.routes";
+import { voteRoutes } from "../modules/Vote/vote.route";
 
 const route = Router();
 const modules = [
@@ -10,6 +12,9 @@ const modules = [
   { path: "/category", route: categoryRoute },
   { path: "/post", route: postRoute },
   { path: "/admin", route: adminRoute },
+
+  {path: "/rating", route: ratingRoutes},
+  {path: "/voting", route: voteRoutes},
 ];
 
 modules.forEach((module) => {
