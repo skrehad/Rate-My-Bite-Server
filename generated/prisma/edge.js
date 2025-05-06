766c1c20efad4ab5e7c24d3e3a28123905fc39d8
Al-amin07 <alsujon2001@gmail.com>
2025-05-07 00:24:38 +0600

    deploying

--- generated/prisma/edge.js
+++ generated/prisma/edge.js
@@ -255,11 +255,12 @@ const config = {
     "db"
   ],
   "activeProvider": "postgresql",
+  "postinstall": false,
   "inlineDatasources": {
     "db": {
       "url": {
         "fromEnvVar": "DATABASE_URL",
-        "value": "postgresql://postgres:postgre@localhost:5432/mydb?schema=public"
+        "value": null
       }
     }
   },
